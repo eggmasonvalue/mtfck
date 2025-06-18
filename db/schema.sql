e6890e4148e04ebd92db5e380e92981dc7ac0ac3
eggmasonvalue <kirubhasshankarkbs@gmail.com>
2025-06-18 21:20:00 +0530

    update schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,21 @@
+CREATE TABLE IF NOT EXISTS stock_master (
+    symbol TEXT PRIMARY KEY,
+    name TEXT,
+    industry TEXT
+);
 
+CREATE TABLE IF NOT EXISTS stock_data (
+    date DATE,
+    symbol TEXT,
+    qty_financed INTEGER,
+    amt_financed REAL,
+    FOREIGN KEY(symbol) REFERENCES stock_master(symbol)
+);
+
+CREATE TABLE IF NOT EXISTS daily_summary (
+    date DATE PRIMARY KEY,
+    total_outstanding_begin REAL,
+    fresh_exposure REAL,
+    exposure_liquidated REAL,
+    net_outstanding_end REAL
+);
